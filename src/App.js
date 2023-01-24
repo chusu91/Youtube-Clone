1ae4b737b619c39ed21615c28d2f5b633efcf603
Lucy <chusu91@gmail.com>
2023-01-24 23:20:12 +0200

    search header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import { Outlet } from "react-router-dom";
-import Header from "./components/Header";
+import SearchHeader from "./components/SearchHeader";
 
 function App() {
   return (
     <>
-      <Header />
+      <SearchHeader />
       <Outlet />
     </>
   );
