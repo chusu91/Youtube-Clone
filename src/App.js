ec68017de38b6bfa155118a2b3436bf0d6b21ff7
Lucy <chusu91@gmail.com>
2023-01-24 22:15:29 +0200

    start the project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,5 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import "./App.css";
-import Home from "./pages/Home";
-import NotFound from "./pages/NotFound";
-import Root from "./pages/Root";
-import VideoPlay from "./pages/VideoPlay";
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Root />,
-    errorElement: <NotFound />,
-    children: [
-      { index: true, element: <Home /> },
-      { path: "/:videoId", element: <VideoPlay /> },
-    ],
-  },
-]);
-
 function App() {
-  return <RouterProvider router={router} />;
+  return <></>;
 }
 
 export default App;
