bc6b3f425f9bc34f6e66cbeb6d928e481e85a146
Lucy <chusu91@gmail.com>
2023-01-25 09:40:30 +0200

    get data with axios in youtube class

--- src/api/youtube.js
+++ src/api/youtube.js
@@ -1,59 +1,21 @@
-export default class Youtube {
-  constructor(apiClient) {
-    this.apiClient = apiClient;
-  }
+import axios from "axios";
+
+export class Youtube {
+  // constructor(apiClient) {
+  //   this.apiClient = apiClient;
+  // }
 
   async search(keyword) {
-    return keyword ? this.#searchByKeyword(keyword) : this.#mostPopular();
+    return keyword ? this.#searchBykeyword(keyword) : this.#popularVideos();
   }
 
-  async channelImageURL(id) {
-    return this.apiClient
-      .channels({
-        params: {
-          part: "snippet",
-          id,
-        },
-      })
-      .then((res) => res.data.items[0].snippet.thumbnails.default.url);
-  }
-
-  async relatedVideos(id) {
-    return this.apiClient
-      .search({
-        params: {
-          part: "snippet",
-          maxResults: "25",
-          type: "video",
-          relatedToVideoId: id,
-        },
-      })
+  async #searchBykeyword(keyword) {
+    return axios
+      .get(`/data/search.json`)
       .then((res) => res.data.items)
       .then((items) => items.map((item) => ({ ...item, id: item.id.videoId })));
   }
-
-  async #searchByKeyword(keyword) {
-    return this.apiClient
-      .search({
-        params: {
-          part: "snippet",
-          maxResults: 25,
-          q: keyword,
-        },
-      })
-      .then((res) => res.data.items)
-      .then((items) => items.map((item) => ({ ...item, id: item.id.videoId })));
-  }
-
-  async #mostPopular() {
-    return this.apiClient
-      .videos({
-        params: {
-          part: "snippet",
-          chart: "mostPopular",
-          maxResults: 25,
-        },
-      })
-      .then((res) => res.data.items);
+  async #popularVideos() {
+    return axios.get(`/data/popular.json`).then((res) => res.data.items);
   }
 }
--- src/pages/Videos.jsx
+++ src/pages/Videos.jsx
@@ -1,5 +1,28 @@
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
 import React from "react";
+import { useParams } from "react-router-dom";
+import { search, Youtube } from "../api/youtube";
+import VideoCard from "../components/VideoCard";
 
 export default function Videos() {
-  return <div>Videos</div>;
+  const { keyword } = useParams();
+  const youtube = new Youtube();
+  const {
+    isLoading,
+    error,
+    data: videos,
+  } = useQuery(["videos", keyword], () => youtube.search(keyword));
+
+  return (
+    <>
+      {videos && (
+        <ul>
+          {videos.map((video) => (
+            <VideoCard key={video.id} video={video} />
+          ))}
+        </ul>
+      )}
+    </>
+  );
 }
